0dcfde924c6727f0afd78766c7efd05f91bd354f
Will Njundong <njundongw@gmail.com>
2022-09-19 22:55:33 -0400

    add apps

---
+++ backend/api/users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(_('username'), max_length=30, blank=False, unique=True)
+    full_name = models.CharField(_('full name'), max_length=100, blank=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
